f3f1628936a38aaac1cd771ea5e12c787a109508
Matthew McDermott <mattmcdermott8@gmail.com>
2025-08-21 14:23:49 -0400

    Trying to fix documentation generatoin; not working

--- build.lua
+++ build.lua
@@ -3,17 +3,17 @@ module  = "notation"
 typesetfiles  = {"notation.dtx"}
 
 scriptfiles   = {}
-installfiles  = {"notation.sty", "notation-*.sty"}
-sourcefiles   = {"notation.dtx","notation.ins"}
+installfiles  = {"notation.sty"}
+sourcefiles   = {"notation.dtx", "notation.ins"}
 docfiles     = {"README.md", "LICENSE", "examples/demo.tex"}
 
 -- Tagging support: `l3build tag v0.1`
 function update_tag(file, content, tagname, tagdate)
   content = string.gsub(content,
-    "\\ProvidesExplPackage%{notationTex%}%%b{}%%b{}",
-    "\\ProvidesExplPackage{notationTex}{"..tagdate.."}{"..string.sub(tagname,2).."}")
+    "\\ProvidesExplPackage%{notation%}%%b{}%%b{}",
+    "\\ProvidesExplPackage{notation}{"..tagdate.."}{"..string.sub(tagname,2).."}")
   content = string.gsub(content,
-    "\\ProvidesFile%{notationTex%.dtx%}%%b[]",
-    "\\ProvidesFile{notationTex.dtx}["..tagdate.." "..string.sub(tagname,2).." AI/ML notation macros + table]")
+    "\\ProvidesFile%{notation%.dtx%}%%b[]",
+    "\\ProvidesFile{notation.dtx}["..tagdate.." "..string.sub(tagname,2).." AI/ML notation macros + table]")
   return content
 end
